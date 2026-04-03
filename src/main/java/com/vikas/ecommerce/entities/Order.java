a090c78a15be0f1e66c09f58aa80d9cb738be151
Vikas <vikasnagabhushan12@gmail.com>
2026-04-03 10:16:22 +0530

    Added Order entity

---
+++ src/main/java/com/vikas/ecommerce/entities/Order.java
@@ -0,0 +1,36 @@
+package com.vikas.ecommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import java.time.LocalDateTime;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@Table(name="orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable = false)
+    User user;
+
+    @Column(nullable = false)
+    double totalPrice;
+
+    LocalDateTime createdAt;
+     @PrePersist
+     public  void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    boolean active=true;
+
+
+}
