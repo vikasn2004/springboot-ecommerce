bba9b7bc67014295592c1480f59bd29235682ddb
Vikas <vikasnagabhushan12@gmail.com>
2026-04-04 10:59:28 +0530

    added orderitem

--- src/main/java/com/vikas/ecommerce/entities/Order.java
+++ src/main/java/com/vikas/ecommerce/entities/Order.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -31,6 +32,9 @@ public class Order {
     }
 
     boolean active=true;
+
+     @OneToMany(mappedBy = "order")
+     List<OrderItem> orderItems;
 
 
 }
---
+++ src/main/java/com/vikas/ecommerce/entities/OrderItem.java
@@ -0,0 +1,37 @@
+package com.vikas.ecommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     Long id;
+
+    long quantity;
+
+    @Column(nullable = false)
+    double price;
+
+   @ManyToOne
+   @JoinColumn(name="product_id")
+    Product product;
+
+   @ManyToOne
+    @JoinColumn(name="order_id")
+    Order order;
+
+
+}
+
+
