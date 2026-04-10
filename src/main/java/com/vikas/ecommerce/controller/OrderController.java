122e235e85a2429ab6344c82566e3b75153b4abf
Vikas <vikasnagabhushan12@gmail.com>
2026-04-10 10:45:04 +0530

    added to end point checks

--- src/main/java/com/vikas/ecommerce/controller/OrderController.java
+++ src/main/java/com/vikas/ecommerce/controller/OrderController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,11 +25,13 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(orderRequestDTO));
     }
     @GetMapping("/orders/all")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<OrderResponseDTO>> getAllOrders(){
         return ResponseEntity.ok(orderService.getAllOrders());
     }
 
     @GetMapping("/orders/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<OrderResponseDTO>> getAllOrders(@PathVariable Long userId){
         return ResponseEntity.ok(orderService.getOrder(userId));
     }
