3c77155dce6dd4b16c0f84a56871f576d3f89d6b
Vikas <vikasnagabhushan12@gmail.com>
2026-04-10 10:53:40 +0530

    added the endpoint check for fetching order of a user

--- src/main/java/com/vikas/ecommerce/service/UserSecurityService.java
+++ src/main/java/com/vikas/ecommerce/service/UserSecurityService.java
@@ -1,4 +1,25 @@
 package com.vikas.ecommerce.service;
 
+import com.vikas.ecommerce.entities.User;
+import com.vikas.ecommerce.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service("orderSecurityService")
+@RequiredArgsConstructor
 public class UserSecurityService {
+
+    //inject user repo
+    private final UserRepository userRepository;
+
+    boolean isOwner(Authentication authentication , Long UserId){
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email).orElse(null);
+        if(user == null){
+            return false;
+        }
+    return user.getId().equals(UserId);
+    }
+
 }
